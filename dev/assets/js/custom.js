9c2de9566ad47a38ef5a49c87b904725edb76d41
OksanaVS-prog <kramarenkoksana@ukr.net>
2025-07-17 00:49:05 +0200

    correction js

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -22,10 +22,10 @@ const header = document.querySelector('header')
 const banner = document.querySelector('swiper-banner')
 const headerHeight = header.offsetHeight
 
-banner.setAttribute('style', '--header-height: ' + headerHeight + 'px')
+banner.setAttribute('style', '--header-height: ' + headerHeight + 'px');
 
-const inputCheckbox = document.querySelectorAll('input[type="checkbox"]')
+const inputCheckbox = document.querySelectorAll('input[type="checkbox"]');
 
-inputCheckbox.forEach(item => {
-  item.closest('.input-box').classList.add('input-box_checkbox')
-})
+inputCheckbox.forEach((item) => {
+ item.closest(".input-box").classList.add("input-box_checkbox");
+});
