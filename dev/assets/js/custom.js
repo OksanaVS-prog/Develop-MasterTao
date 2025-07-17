caead7050bce5c4b4ecd07ffb614eb71bde699a6
OksanaVS-prog <kramarenkoksana@ukr.net>
2025-07-17 15:53:00 +0200

    correction errors in js and media

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -19,13 +19,14 @@ const swiperBanner = new Swiper('.swiper-banner', {
 });
 
 const header = document.querySelector('header')
-const banner = document.querySelector('swiper-banner')
+const banner = document.querySelector('.swiper-banner')
 const headerHeight = header.offsetHeight
 
-banner.setAttribute('style', '--header-height: ' + headerHeight + 'px');
+banner.setAttribute('style', '--header-height: ' + headerHeight + 'px')
 
 const inputCheckbox = document.querySelectorAll('input[type="checkbox"]');
 
 inputCheckbox.forEach((item) => {
- item.closest(".input-box").classList.add("input-box_checkbox");
+item.closest('input-box').classList.add('input-box_checkbox')
+
 });
